b286166f6cef19be0a4f22d87e043a7ec9dcc1ba
Simon Trény <simon.treny@gmail.com>
2020-03-23 23:17:40 +0100

    Remove onChange parameter in hooks as it can easily be replaced by useEffect(). Also make deps parameter of useComputedObservable mandatory

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,19 +1,16 @@
 import { useEffect, useMemo, useState } from "react";
-import { Listener, Observable } from "./observable";
+import { Observable } from "./observable";
 
-export function useObservable<T>(observable: Observable<T>, onChange?: Listener<T>): T {
+export function useObservable<T>(observable: Observable<T>): T {
 	const [, forceRender] = useState({});
 
 	useEffect(() => {
-		return observable.onChange((val, prevVal) => {
-			forceRender({});
-			onChange && onChange(val, prevVal);
-		});
+		return observable.onChange(() => forceRender({}));
 	}, [observable]);
 
 	return observable.get();
 }
 
-export function useComputedObservable<T>(compute: () => Observable<T>, deps: any[] = [], onChange?: Listener<T>): T {
-	return useObservable(useMemo(compute, deps), onChange);
+export function useComputedObservable<T>(compute: () => Observable<T>, deps: any[]): T {
+	return useObservable(useMemo(compute, deps));
 }
