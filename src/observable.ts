d2050eff8cbf9aba26b73ba4765f30557b3e2507
Simon Trény <simon.treny@gmail.com>
2020-05-17 23:05:49 +0200

    Refactoring of Observable. It is now easier to understand. Changes are also atomic now: listeners are notified only once when an observable depends on several others, depending themselves on a common ancestor (i.e. diamond-shaped graph).

--- src/observable.ts
+++ src/observable.ts
@@ -1,54 +1,163 @@
-import memoizeOne from "memoize-one";
-
 export type Listener<T> = (val: T, prevVal: T) => void;
 export type Unsubscriber = () => void;
 
 export type ObservableValue<T> = T extends Observable<infer U> ? U : T;
 export type ObservableValues<T> = { [K in keyof T]: ObservableValue<T[K]> };
 
-export class Observable<T> {
-	private _val: T | Observable<T>;
-	private _valUnsubscriber: Unsubscriber | undefined;
+class BaseObservable<T> {
+	private _val!: T;
+	private _inputs: BaseObservable<any>[] = [];
+	private _outputs: BaseObservable<any>[] = [];
 	private _listeners: Listener<T>[] = [];
+	private _attachedToInputs = false;
+	private _dirty = false;
 
-	constructor(val: T | Observable<T>) {
-		this._val = val;
+	constructor() {
+		// Note: this._val is not assigned here.
+		// Subclasses should call _set() in their constructor
 	}
 
 	get(): T {
-		return this._val instanceof Observable ? this._val.get() : this._val;
+		return this._val;
 	}
 
-	protected _set(val: T | Observable<T>) {
+	protected _set(val: T) {
 		if (this._val !== val) {
-			this.unlistenToValueChanges();
-
-			const prevVal = this.get();
+			const prevVal = this._val;
 			this._val = val;
-			const newVal = this.get();
+			this._dirty = false;
 
-			this.listenToValueChanges();
+			// Invalidate outputs before notifying listeners.
+			// This way, if get() is called on an outputs from a listener, it'll be already up-to-date
+			for (const output of this._outputs) {
+				output.invalidate();
+			}
 
-			if (newVal !== prevVal) {
-				this.notifyListeners(newVal, prevVal);
+			// Notify listeners
+			for (const listener of this._listeners) {
+				listener(val, prevVal);
+			}
+
+			// Refresh outputs that may have changed
+			for (const output of this._outputs) {
+				output._set(output.get());
 			}
 		}
 	}
 
 	onChange(listener: Listener<T>): Unsubscriber {
 		this._listeners.push(listener);
-		this.listenToValueChanges();
+		this.attachToInputs();
 
 		let listenerRemoved = false;
 		return () => {
 			if (!listenerRemoved) {
 				listenerRemoved = true;
 				this._listeners.splice(this._listeners.indexOf(listener), 1);
-				if (this._listeners.length === 0) {
-					this.unlistenToValueChanges();
-				}
+				this.detachFromInputs();
 			}
 		};
+	}
+
+	protected shouldEvaluate(): boolean {
+		return !this._attachedToInputs || this._dirty;
+	}
+
+	protected addInput(input: BaseObservable<any>) {
+		this._inputs.push(input);
+		if (this._attachedToInputs) {
+			this.attachToInput(input);
+		}
+	}
+
+	protected removeInput(input: BaseObservable<any>) {
+		this._inputs.splice(this._inputs.indexOf(input), 1);
+		if (this._attachedToInputs) {
+			this.detachFromInput(input);
+		}
+	}
+
+	private shouldAttachToInputs(): boolean {
+		// Only attach to inputs when at least one listener is subscribed to the observable or to one of its outputs.
+		// This is done to avoid unused observables being references by their inputs, preventing garbage-collection.
+		return this._listeners.length > 0 || this._outputs.length > 0;
+	}
+
+	private attachToInputs() {
+		if (!this._attachedToInputs && this.shouldAttachToInputs()) {
+			for (const input of this._inputs) {
+				this.attachToInput(input);
+				input.attachToInputs();
+			}
+
+			// Since the observable was not attached to its inputs, its value may be outdated.
+			// Refresh it so that onChange() will be called with the correct prevValue the next time an input changes.
+			this._val = this.get();
+			this._dirty = false;
+			this._attachedToInputs = true;
+		}
+	}
+
+	private detachFromInputs() {
+		if (this._attachedToInputs && !this.shouldAttachToInputs()) {
+			this._attachedToInputs = false;
+			for (const input of this._inputs) {
+				this.detachFromInput(input);
+				input.detachFromInputs();
+			}
+		}
+	}
+
+	private attachToInput(input: BaseObservable<any>) {
+		input._outputs.push(this);
+		if (input._dirty) {
+			this.invalidate();
+		}
+	}
+
+	private detachFromInput(input: BaseObservable<any>) {
+		input._outputs.splice(input._outputs.indexOf(this), 1);
+	}
+
+	private invalidateOutputs() {
+		for (const output of this._outputs) {
+			output.invalidate();
+		}
+	}
+
+	private invalidate() {
+		if (!this._dirty) {
+			this._dirty = true;
+			this.invalidateOutputs();
+		}
+	}
+}
+
+export class Observable<T> extends BaseObservable<T> {
+	private _valInput: Observable<T> | undefined;
+
+	constructor(val: T | Observable<T>) {
+		super();
+		this._set(val);
+	}
+
+	get(): T {
+		return this.shouldEvaluate() && this._valInput ? this._valInput.get() : super.get();
+	}
+
+	protected _set(val: T | Observable<T>) {
+		if (this._valInput && this._valInput !== val) {
+			this.removeInput(this._valInput);
+			this._valInput = undefined;
+		}
+
+		if (val instanceof Observable) {
+			this._valInput = val;
+			this.addInput(val);
+			super._set(val.get());
+		} else {
+			super._set(val);
+		}
 	}
 
 	transform<U>(transform: (val: T) => U | Observable<U>): Observable<U> {
@@ -80,7 +189,6 @@ export class Observable<T> {
 	static latest<T>(...observables: Observable<T>[]): Observable<T> {
 		let prevValues: T[] | undefined;
 		return new ComputedObservable(observables, values => {
-			debugger;
 			const val = !prevValues ? values[0] : values.find((it, index) => it !== prevValues![index])!;
 			prevValues = values;
 			return val;
@@ -107,31 +215,6 @@ export class Observable<T> {
 			});
 		});
 	}
-
-	protected hasListeners(): boolean {
-		return this._listeners.length > 0;
-	}
-
-	private notifyListeners(val: T, prevVal: T): void {
-		this._listeners.slice().forEach(it => it(val, prevVal)); // TODO: optimize slice ?
-	}
-
-	private listenToValueChanges(): void {
-		if (!this.hasListeners()) {
-			// TODO: Explain
-			return;
-		}
-		if (!this._valUnsubscriber && this._val instanceof Observable) {
-			this._valUnsubscriber = this._val.onChange((v, p) => this.notifyListeners(v, p));
-		}
-	}
-
-	private unlistenToValueChanges(): void {
-		if (this._valUnsubscriber) {
-			this._valUnsubscriber();
-			this._valUnsubscriber = undefined;
-		}
-	}
 }
 
 export class WritableObservable<T> extends Observable<T> {
@@ -149,60 +232,53 @@ export class WritableObservable<T> extends Observable<T> {
 }
 
 class ComputedObservable<T, U extends Observable<any>[]> extends Observable<T> {
-	private _inputs: U;
-	private _inputUnsubscribers: Unsubscriber[] = [];
-	private _compute: (...vals: any[]) => T | Observable<T>;
+	private _compute: (vals: ObservableValues<U>) => T | Observable<T>;
+	private _computeInputs: U;
 
-	constructor(inputs: U, compute: (vals: ObservableValues<U>) => T | Observable<T>) {
-		super((undefined as unknown) as T);
-		this._inputs = inputs;
-		this._compute = memoizeOne((...values) => compute(values as ObservableValues<U>));
-		this.updateValue();
+	constructor(computeInputs: U, compute: (vals: ObservableValues<U>) => T | Observable<T>) {
+		const memoizedCompute = memoize(compute);
+		super(memoizedCompute(computeInputs.map(input => input.get()) as ObservableValues<U>));
+		this._compute = memoizedCompute;
+		this._computeInputs = computeInputs;
+		computeInputs.forEach(input => this.addInput(input));
 	}
 
 	get(): T {
-		if (!this.hasListeners()) {
-			const val = this.computeValue();
+		if (this.shouldEvaluate()) {
+			const val = this._compute(this._computeInputs.map(input => input.get()) as ObservableValues<U>);
 			return val instanceof Observable ? val.get() : val;
 		} else {
 			return super.get();
 		}
 	}
-
-	onChange(listener: Listener<T>): Unsubscriber {
-		if (!this.hasListeners()) {
-			this.updateValue();
-			this.listenToInputChanges();
-		}
-
-		const unsubscribe = super.onChange(listener);
-
-		return () => {
-			unsubscribe();
-			if (!this.hasListeners()) {
-				this.unlistenToInputChanges();
-			}
-		};
-	}
-
-	protected listenToInputChanges(): void {
-		this._inputUnsubscribers = this._inputs.map(it => it.onChange(() => this.updateValue()));
-	}
-
-	protected unlistenToInputChanges(): void {
-		this._inputUnsubscribers.forEach(it => it());
-		this._inputUnsubscribers = [];
-	}
-
-	private computeValue(): T | Observable<T> {
-		return this._compute(...this._inputs.map(it => it.get()));
-	}
-
-	private updateValue(): void {
-		this._set(this.computeValue());
-	}
 }
 
 export function observable<T>(val: T | Observable<T>): WritableObservable<T> {
 	return new WritableObservable(val);
+}
+
+export function memoize<T extends any[], U>(func: (args: T) => U): (args: T) => U {
+	let lastArgs: T | undefined;
+	let lastResult!: U;
+
+	return (args: T) => {
+		let argsHaveChanged = false;
+		if (!lastArgs || args.length !== lastArgs.length) {
+			argsHaveChanged = true;
+		} else {
+			for (let i = 0; i < args.length; i++) {
+				if (args[i] !== lastArgs[i]) {
+					argsHaveChanged = true;
+					break;
+				}
+			}
+		}
+
+		if (argsHaveChanged) {
+			lastArgs = args;
+			lastResult = func(args);
+		}
+
+		return lastResult;
+	};
 }
